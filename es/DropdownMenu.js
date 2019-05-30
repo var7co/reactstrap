d308a89ccba28d72258f2feb0eac409a8b1d7639
Mario Casciaro <mariocasciaro@gmail.com>
2019-05-30 15:49:34 +0100

    Build

---
+++ es/DropdownMenu.js
@@ -0,0 +1,119 @@
+import _extends from "@babel/runtime/helpers/esm/extends";
+import _objectSpread from "@babel/runtime/helpers/esm/objectSpread";
+import _objectWithoutPropertiesLoose from "@babel/runtime/helpers/esm/objectWithoutPropertiesLoose";
+import _inheritsLoose from "@babel/runtime/helpers/esm/inheritsLoose";
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import classNames from 'classnames';
+import { Popper } from 'react-popper';
+import { DropdownContext } from './DropdownContext';
+import { mapToCssModules, tagPropType, targetPropType, getTarget } from './utils';
+var propTypes = {
+  tag: tagPropType,
+  children: PropTypes.node.isRequired,
+  right: PropTypes.bool,
+  flip: PropTypes.bool,
+  modifiers: PropTypes.object,
+  className: PropTypes.string,
+  cssModule: PropTypes.object,
+  persist: PropTypes.bool,
+  container: targetPropType
+};
+var defaultProps = {
+  tag: 'div',
+  flip: true,
+  container: 'inline'
+};
+var noFlipModifier = {
+  flip: {
+    enabled: false
+  }
+};
+var directionPositionMap = {
+  up: 'top',
+  left: 'left',
+  right: 'right',
+  down: 'bottom'
+};
+
+var DropdownMenu =
+/*#__PURE__*/
+function (_React$Component) {
+  _inheritsLoose(DropdownMenu, _React$Component);
+
+  function DropdownMenu() {
+    return _React$Component.apply(this, arguments) || this;
+  }
+
+  var _proto = DropdownMenu.prototype;
+
+  _proto.render = function render() {
+    var _this = this;
+
+    var _this$props = this.props,
+        className = _this$props.className,
+        cssModule = _this$props.cssModule,
+        right = _this$props.right,
+        tag = _this$props.tag,
+        flip = _this$props.flip,
+        modifiers = _this$props.modifiers,
+        persist = _this$props.persist,
+        container = _this$props.container,
+        attrs = _objectWithoutPropertiesLoose(_this$props, ["className", "cssModule", "right", "tag", "flip", "modifiers", "persist", "container"]);
+
+    var classes = mapToCssModules(classNames(className, 'dropdown-menu', {
+      'dropdown-menu-right': right,
+      show: this.context.isOpen
+    }), cssModule);
+    var Tag = tag;
+
+    if (persist || this.context.isOpen && !this.context.inNavbar) {
+      var position1 = directionPositionMap[this.context.direction] || 'bottom';
+      var position2 = right ? 'end' : 'start';
+      var poperPlacement = position1 + "-" + position2;
+      var poperModifiers = !flip ? _objectSpread({}, modifiers, noFlipModifier) : modifiers;
+      var popper = React.createElement(Popper, {
+        placement: poperPlacement,
+        modifiers: poperModifiers
+      }, function (_ref) {
+        var ref = _ref.ref,
+            style = _ref.style,
+            placement = _ref.placement;
+        return React.createElement(Tag, _extends({
+          tabIndex: "-1",
+          role: "menu",
+          ref: ref,
+          style: style
+        }, attrs, {
+          "aria-hidden": !_this.context.isOpen,
+          className: classes,
+          "x-placement": placement
+        }));
+      });
+
+      if (this.context.menuContainer) {
+        return ReactDOM.createPortal(popper, getTarget(this.context.menuContainer));
+      } else {
+        return popper;
+      }
+    }
+
+    return React.createElement(Tag, _extends({
+      tabIndex: "-1",
+      role: "menu"
+    }, attrs, {
+      "aria-hidden": !this.context.isOpen,
+      className: classes,
+      "x-placement": attrs.placement
+    }));
+  };
+
+  return DropdownMenu;
+}(React.Component);
+
+;
+DropdownMenu.propTypes = propTypes;
+DropdownMenu.defaultProps = defaultProps;
+DropdownMenu.contextType = DropdownContext;
+export default DropdownMenu;
